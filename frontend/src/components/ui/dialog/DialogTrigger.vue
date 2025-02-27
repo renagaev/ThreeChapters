1131654845f0c9c62c41d7299242da997f7d6f4f
Нагаев Ренат Евгеньевич <r.nagaev@tbank.ru>
2025-02-27 22:39:00 +0300

    book progress dialog

---
+++ frontend/src/components/ui/dialog/DialogTrigger.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {DialogTrigger, type DialogTriggerProps} from 'reka-ui'
+
+const props = defineProps<DialogTriggerProps & { disabled?: boolean }>()
+</script>
+
+<template>
+  <DialogTrigger v-if="!props.disabled" v-bind="props">
+    <slot/>
+  </DialogTrigger>
+  <div v-else>
+    <slot/>
+  </div>
+</template>
