f038866ee6b786fa4ec0909b6ad708eb5526fcca
Нагаев Ренат Евгеньевич <r.nagaev@tbank.ru>
2025-03-01 21:11:18 +0300

    always scroll to top

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -37,6 +37,9 @@ export const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+  scrollBehavior(to, from, savedPosition) {
+    return {top: 0}
+  }
+})
 
 export default router;
